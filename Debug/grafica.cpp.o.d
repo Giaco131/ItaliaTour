e4159d579077e6ab193a6fb61ba4003b2e983ce3
Manbk <manbk120799@gmail.com>
2016-06-17 11:34:45 +0200

    MODIFICA
    aggiunto spazio alla funzione car

---
+++ Debug/grafica.cpp.o.d
@@ -0,0 +1,14 @@
+Debug/grafica.cpp.o: grafica.cpp librerie.h grafica.h record.h \
+ letturaFile.h funzioniGenerali.h sqlite3.h
+
+librerie.h:
+
+grafica.h:
+
+record.h:
+
+letturaFile.h:
+
+funzioniGenerali.h:
+
+sqlite3.h:
